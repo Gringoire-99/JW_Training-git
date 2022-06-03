bf76a9e1dd19384ed7ed7440d29686a7f9984c6a
gringoire <q15396079922@sina.com>
2022-06-03 19:59:34 +0800

    update 2022.6.3

---
+++ src/test/java/com/example/librarymanagementsystem/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.example.librarymanagementsystem.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+interface UserDao {
+}
---
+++ src/test/java/com/example/librarymanagementsystem/model/User.java
@@ -0,0 +1,9 @@
+package com.example.librarymanagementsystem.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("librarymanagementsystem.book")
+public class User {
+}
