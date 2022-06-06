5257971b25ad6aa38233888c59e805209d5dd904
gringoire <q15396079922@sina.com>
2022-06-06 10:40:56 +0800

    更新了导航栏

--- vuecli/vue_test2/src/main.js
+++ vuecli/vue_test2/src/main.js
@@ -8,6 +8,11 @@ import {store} from "@/store";
 import App from './App.vue'
 import Router from "@/router";
 const app = createApp(App)
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus)
 app.use(ElementPlus, {
     locale: zhCn,
 })
